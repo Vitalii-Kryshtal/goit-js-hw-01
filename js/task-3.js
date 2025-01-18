78679881c670dd237470f661f26a32a9efda7170
Vitalii Kryshtal <krishtal.vtalk@ukr.net>
2025-01-18 17:11:00 +0200

    js files fixed

--- js/task-3.js
+++ js/task-3.js
@@ -1,5 +1,5 @@
 function getElementWidth(content, padding, border) {
-    return parceFloat(content) + parseFloat(padding) * 2 + parseFloat(border) * 2;
+    return parseFloat(content) + parseFloat(padding) * 2 + parseFloat(border) * 2;
 }
 
 
